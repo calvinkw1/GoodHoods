b0caebd8c10ddaa10932d0bc708f9dabbef0ec4c
DoyleMcpoyle <pdoyleson@gmail.com>
2015-03-07 15:46:43 -0800

    users controller and views

--- goodhoods/app/models/user.rb
+++ goodhoods/app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   has_many :hoods, through: :searches
   has_many :searches
   has_secure_password
+  validates :username, presence: true
+  validates :username, uniqueness: true
 end
