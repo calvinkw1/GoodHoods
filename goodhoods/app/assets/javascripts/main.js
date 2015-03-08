125a7254464e6e669c651fc293805d5eff3408e2
Calvin Wang <calvinkw1@gmail.com>
2015-03-07 21:09:20 -0800

    added proper appending of search results

--- goodhoods/app/assets/javascripts/main.js
+++ goodhoods/app/assets/javascripts/main.js
@@ -2,13 +2,16 @@ $(document).ready(function() {
   
   $("form").submit(function(e) {
     e.preventDefault();
+    $("#charts").empty();
     var city = $("#city").val();
     var state = $("#state").val();
     var url = "/search";
     $.getJSON(url, {city:city, state:state}, function(data) {
-      charts = data.demographics.response.charts;
-      $("div").append("<h5>" + charts.chart[0].name + "</h5>");
-      $("div").append("<div><img src=" + charts.chart[0].url + "></div>");
+      charts = data.demographics.response.charts.chart;
+      for (i = 0; i < charts.length; i++) {
+        $("#charts").append("<h5>" + charts[i].name + "</h5>");
+        $("#charts").append("<div><img src=" + charts[i].url + "></div>");
+      }
       console.log(charts);
     });
   });
