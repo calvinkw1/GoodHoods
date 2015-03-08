a5ba08c6cad49554b26040e48a2af33fea7c7ef6
Calvin Wang <calvinkw1@gmail.com>
2015-03-07 20:49:29 -0800

    zillow api chart is showing up upon search

--- goodhoods/app/assets/javascripts/main.js
+++ goodhoods/app/assets/javascripts/main.js
@@ -6,13 +6,10 @@ $(document).ready(function() {
     var state = $("#state").val();
     var url = "/search";
     $.getJSON(url, {city:city, state:state}, function(data) {
-      // for (i = 0; i < data.length; i++) {
       charts = data.demographics.response.charts;
-        $("ul").append("<div><img src=" + charts.chart[0].url + "></div>");
-        // charts.chart[0].url
-        console.log(charts);
-        // $("ul").append("<li>data[i]</li>");
-      // }
+      $("div").append("<h5>" + charts.chart[0].name + "</h5>");
+      $("div").append("<div><img src=" + charts.chart[0].url + "></div>");
+      console.log(charts);
     });
   });
 
