30554eb7deb0e8c122c11ee736418c1cbbef5074
Michael Pospisil <michaelcpospisil@gmail.com>
2015-03-10 09:59:36 -0700

    adding maps with neighborhood listing

--- goodhoods/app/assets/javascripts/main.js
+++ goodhoods/app/assets/javascripts/main.js
@@ -18,16 +18,26 @@ function initialize() {
   }; 
   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-  $.getJSON('/californiaZillow.json', function(hoods) {
-  console.log(hoods);
-  map.data.addGeoJson(hoods);
+  $.getJSON('/CaliZillowSimp.json', function(hoods) {
+  console.log(hoods.features[0].properties.CITY);
+  // console.log(hoods.features[0]);
+  var labels = hoods;
+console.log(hoods);
+  for (i = 0; i < labels.features.length; i++) {
+   console.log($("#search_input").toArray());
+    $("#labels").append("<li>" + labels.features[i].properties.CITY + ",  " + labels.features[i].properties.NAME + "</li>");
+  }
 
-  });
-    var featureStyle = {
-      fillColor: 'green',
-      strokeColor: '#E9DBE8',
-      strokeWeight: 2
-    };
+  // map.data.addGeoJson(hoods);
+ }); 
+
+  
+
+  var featureStyle = {
+    fillColor: 'green',
+    strokeColor: '#E9DBE8',
+    strokeWeight: 2
+  };
    map.data.setStyle(featureStyle); 
    map.data.addListener('mouseover', function(event) {
    map.data.overrideStyle(event.feature, {fillColor: 'red'});
@@ -43,6 +53,7 @@ google.maps.event.addDomListener(window, 'load', initialize);
 
 
   $(document).ready(function(){
+
     
 // function searchBox() {  
    console.log($("#search_input").toArray());
